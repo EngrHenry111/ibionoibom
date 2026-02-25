8d9ced5773c73d7f4b942c8eabe75f8606885142
EngrHenry111 <henryengrakpan@gmail.com>
2026-02-25 20:03:29 +0100

    adding express uploads routes

--- src/app.js
+++ src/app.js
@@ -16,6 +16,8 @@ app.use(cors());
 app.use(express.json());
 app.use("/uploads", express.static("uploads"));
 
+
+app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 // app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
 
 // app.use("/uploads", express.static(path.resolve("uploads")));
