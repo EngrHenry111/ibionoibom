c989efe1511446ef399060181ece4bae7cdeaaed
EngrHenry111 <henryengrakpan@gmail.com>
2026-02-25 20:04:53 +0100

    removing unwanted route

--- src/app.js
+++ src/app.js
@@ -14,7 +14,6 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads"));
 
 
 app.use("/uploads", express.static(path.join(process.cwd(), "uploads")));
