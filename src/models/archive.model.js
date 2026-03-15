b58bb23a787ea7a9168d9e12f52a80dd92ea327c
EngrHenry111 <henryengrakpan@gmail.com>
2026-03-15 15:40:09 +0100

    archive model  correction

--- src/models/archive.model.js
+++ src/models/archive.model.js
@@ -1,61 +1,41 @@
+import mongoose from "mongoose";
+
 const archiveSchema = new mongoose.Schema(
-  {
-    title: String,
-    description: String,
-    category: String,
-    year: Number,
-    tags: [String],
-    referenceId: mongoose.Schema.Types.ObjectId
-  },
-  { timestamps: true }
+{
+title: {
+type: String,
+required: true
+},
+
+description: String,
+
+category: {
+type: String,
+enum: [
+"leader",
+"document",
+"news",
+"policy",
+"report"
+]
+},
+
+year: Number,
+
+tags: [String],
+
+fileUrl: String,
+
+referenceId: mongoose.Schema.Types.ObjectId
+},
+{ timestamps: true }
 );
 
-archiveSchema.index({ year: -1 });
-archiveSchema.index({ category: 1 });
-archiveSchema.index({ title: "text", description: "text" });
+/* SEARCH INDEX */
+archiveSchema.index({
+title: "text",
+description: "text",
+tags: "text"
+});
 
-export default mongoose.model("Archive", archiveSchema);
-
-// import mongoose from "mongoose";
-
-// const archiveSchema = new mongoose.Schema(
-// {
-// title: {
-// type: String,
-// required: true
-// },
-
-// description: String,
-
-// category: {
-// type: String,
-// enum: [
-// "leader",
-// "document",
-// "news",
-// "policy",
-// "report"
-// ]
-// },
-
-// year: Number,
-
-// tags: [String],
-
-// fileUrl: String,
-
-// referenceId: mongoose.Schema.Types.ObjectId
-// },
-// { timestamps: true }
-// );
-
-// /* SEARCH INDEX */
-// archiveSchema.index({
-// title: "text",
-// description: "text",
-// tags: "text"
-// });
-
-// export default mongoose.model("Archive", archiveSchema); 
-
-
+export default mongoose.model("Archive", archiveSchema); 
