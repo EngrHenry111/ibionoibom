75fa711edd4b04e63420e2687d6cf2e888eab107
EngrHenry111 <henryengrakpan@gmail.com>
2026-03-19 16:12:25 +0100

    bursary route correction

--- src/routes/bursary.routes.js
+++ src/routes/bursary.routes.js
@@ -1,5 +1,5 @@
 import { generateLetter, verifyBursary } from "../controllers/bursary.controller.js";
-import { protectStudent } from "../middlewares/studentAuth.js";
+import { protectStudent } from "../middlewares/studentAuth.middleware.js";
 import { getMyApplications } from "../controllers/bursary.controller.js";
 
 router.get("/my", protectStudent, getMyApplications);
