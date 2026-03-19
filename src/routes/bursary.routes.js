91eaf98f91509874c9e39d003f37c74d5766d789
EngrHenry111 <henryengrakpan@gmail.com>
2026-03-19 16:26:31 +0100

    exporting bursary router

--- src/routes/bursary.routes.js
+++ src/routes/bursary.routes.js
@@ -1,9 +1,12 @@
 import { generateLetter, verifyBursary } from "../controllers/bursary.controller.js";
 import { protectStudent } from "../middlewares/studentAuth.middleware.js";
 import { getMyApplications } from "../controllers/bursary.controller.js";
+import express from "express";
 
+const router = express.Router();
 router.get("/my", protectStudent, getMyApplications);
 router.get("/letter/:id", generateLetter);
 
 router.get("/verify/:code", verifyBursary)
 
+export default router;
