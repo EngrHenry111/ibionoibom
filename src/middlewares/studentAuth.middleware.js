5192510117572651753d042507928b3bbc2ac066
EngrHenry111 <henryengrakpan@gmail.com>
2026-03-21 08:25:47 +0100

    middleware next

--- src/middlewares/studentAuth.middleware.js
+++ src/middlewares/studentAuth.middleware.js
@@ -1,3 +1,31 @@
+import jwt from "jsonwebtoken";
+import Student from "../models/student.model.js";
+
+export const protectStudent = async (req, res, next) => {
+  try {
+    const token = req.headers.authorization?.split(" ")[1];
+
+    if (!token) {
+      return res.status(401).json({ message: "No token" });
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+    req.user = await Student.findById(decoded.id).select("-password");
+
+    if (!req.user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+
+    next();
+
+  } catch (error) {
+    console.error("AUTH ERROR:", error);
+    res.status(401).json({ message: "Unauthorized" });
+  }
+};
+
+
 // export const protectStudent = async (req, res, next) => {
 //   let token;
 
@@ -25,31 +53,31 @@
 
 
 
-import jwt from "jsonwebtoken";
-import Student from "../models/student.model.js";
+// import jwt from "jsonwebtoken";
+// import Student from "../models/student.model.js";
 
-export const protectStudent = async (req, res, next) => {
-  let token;
+// export const protectStudent = async (req, res, next) => {
+//   let token;
 
-  if (req.headers.authorization?.startsWith("Bearer")) {
-    token = req.headers.authorization.split(" ")[1];
+//   if (req.headers.authorization?.startsWith("Bearer")) {
+//     token = req.headers.authorization.split(" ")[1];
 
-    try {
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+//     try {
+//       const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
-      req.user = await Student.findById(decoded.id).select("-password");
+//       req.user = await Student.findById(decoded.id).select("-password");
 
-      next();
-    } catch (error) {
-      return res.status(401).json({
-        message: "Not authorized",
-      });
-    }
-  }
+//       next();
+//     } catch (error) {
+//       return res.status(401).json({
+//         message: "Not authorized",
+//       });
+//     }
+//   }
 
-  if (!token) {
-    return res.status(401).json({
-      message: "No token",
-    });
-  }
-};
+//   if (!token) {
+//     return res.status(401).json({
+//       message: "No token",
+//     });
+//   }
+// };
