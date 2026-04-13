12b292839c0c424f79099f15d743ebc8ad7a3c76
EngrHenry111 <henryengrakpan@gmail.com>
2026-04-13 21:55:17 +0100

    redirect mobile

--- src/routes/og.routes.js
+++ src/routes/og.routes.js
@@ -33,7 +33,11 @@ router.get("/og/news/:id", async (req, res) => {
         <meta name="twitter:card" content="summary_large_image" />
 
         <!-- ✅ MOBILE SAFE REDIRECT -->
-        <meta http-equiv="refresh" content="0; url=https://ibionoibomlga.com/news/${news._id}" />
+        <script>
+        setTimeout(() => {
+            window.location.href = "https://ibionoibomlga.com/news/${news._id}";
+        }, 3000);
+        </script>
       </head>
       <body></body>
     </html>
